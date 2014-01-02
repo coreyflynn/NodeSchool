c6dbc76f11f5cc9ff83f7dd01ef5a38b08c5d5f1
Corey Flynn <flynn.corey@gmail.com>
2014-01-02 10:43:22 -0500

    timserver

--- learnyounode/httpClient.js
+++ learnyounode/httpClient.js
@@ -4,6 +4,6 @@ var http = require('http');
 url = process.argv[2];
 http.get(url,function(response){
   response.setEncoding('utf8');
-  response.on('data', console.log(data));
+  response.on('data', function(data) {console.log(data);});
   response.on('error', console.error);
 });
--- learnyounode/timeserver.js
+++ learnyounode/timeserver.js
@@ -7,16 +7,20 @@ var port = process.argv[2];
 var server = net.createServer(function(socket){
     var d = new Date();
     var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    var day = d.getDate();
-    var hours = d.getHours();
+    var month = add_zero(d.getMonth() + 1);
+    var day = add_zero(d.getDate());
+    var hours = add_zero(d.getHours());
     var minutes = d.getMinutes();
-    hours = '0' + hours
     
-    var date_string = year + '-' + month + '-' + day  + ' ' + hours + ':' + minutes + '\n'; 
+    var date_string = year + '-' + month + '-' + day  + ' ' + hours + ':' + minutes + '\n';
     socket.end(date_string);
     
 });
+
+function add_zero(input){
+	input = input.toString();
+	return (input.length == 1) ? '0' + input : input;
+}
 
 //start the server
 server.listen(port);
